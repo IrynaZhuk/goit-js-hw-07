a0078688f2c6b093c72f912bbc184d7f92aab7a8
IrynaZhuk <irynazh94@gmail.com>
2022-05-29 20:43:48 +0300

    firstVersionTask2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallaryContainer = document.querySelector(".gallery");
+const items = createGallaryItems(galleryItems);
+
+gallaryContainer.insertAdjacentHTML('beforeend', items);
+
+function createGallaryItems(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  }).join("")  
+}
+
+gallaryContainer.addEventListener('click', onGallaryContainerClickHandler)
+
+function onGallaryContainerClickHandler (event) {
+  event.preventDefault();
+
+  const bigImg = new SimpleLightbox('.gallery a');
+  bigImg.next();
+
+//   $('.gallery a').on('open.simplelightbox', function(){
+//     captionsData: "alt" = event.target.description;
+// })
+}
